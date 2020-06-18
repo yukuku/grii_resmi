9127abb776fa2ef409910f203db53cbddbaa1b5d
Yuku on yuku9 <yukuku@gmail.com>
2020-06-18 19:26:15 +0800

    Pillar list of articles and open articles

--- main_app/lib/pillar.dart
+++ main_app/lib/pillar.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:grii_resmi/pillar_api.dart';
 
 class PillarHome extends StatefulWidget {
@@ -35,17 +37,85 @@ class _PillarHomeState extends State<PillarHome> {
             return Center(child: CircularProgressIndicator());
           }
           final items = snapshot.data;
+
+          String truncate(String s) {
+            if (s.length > 150) {
+              return s.substring(0, 150) + '...';
+            } else {
+              return s;
+            }
+          }
+
           return ListView.builder(
+            padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
             itemCount: items.length,
             itemBuilder: (context, index) {
               final item = items[index];
 
               return ListTile(
                 title: Text(item.title),
-                subtitle: Text(item.snippet),
-                dense: true,
+                subtitle: HtmlWidget(truncate(item.snippet)),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                    return PillarArticlePage(item);
+                  }));
+                },
               );
             },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PillarArticlePage extends StatefulWidget {
+  final ArticleBrief articleBrief;
+
+  PillarArticlePage(this.articleBrief, {Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PillarArticlePageState();
+  }
+}
+
+class _PillarArticlePageState extends State<PillarArticlePage> {
+  final _controller = StreamController<ArticleFull>(); // ignore: close_sinks
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  _init() {
+    getArticle(widget.articleBrief.id, _controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: widget.key,
+      appBar: AppBar(
+        title: Text(widget.articleBrief.title),
+      ),
+      body: StreamBuilder<ArticleFull>(
+        stream: _controller.stream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error ${snapshot.error}'));
+          }
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final item = snapshot.data;
+          return SingleChildScrollView(
+            padding: EdgeInsets.all(16.0),
+            child: HtmlWidget(
+              item.body,
+              textStyle: TextStyle(fontSize: 16.0),
+            ),
           );
         },
       ),
--- main_app/lib/pillar_api.dart
+++ main_app/lib/pillar_api.dart
@@ -6,11 +6,12 @@ import 'package:grii_resmi/installation_id.dart';
 import 'package:http/http.dart' as http;
 
 class ArticleBrief {
+  final int id;
   final String name;
   final String title;
   final String snippet;
 
-  ArticleBrief({this.name, this.title, this.snippet});
+  ArticleBrief({this.id, this.name, this.title, this.snippet});
 }
 
 class ArticleFull {
@@ -43,8 +44,36 @@ void getLatestArticles(StreamController<List<ArticleBrief>> controller) async {
     if (resp.statusCode == 200) {
       final root = json.decode(resp.body);
       final List<dynamic> items = root['articles']['items'];
-      final list = items.map((item) => ArticleBrief(name: item['name'], title: item['title'], snippet: item['snippet'])).toList();
+      final list = items.map((item) => ArticleBrief(id: item['_id'], name: item['name'], title: item['title'], snippet: item['snippet'])).toList();
       controller.add(list);
+      controller.close();
+    } else {
+      throw Exception("bad status code ${resp.statusCode}");
+    }
+  } catch (e) {
+    controller.addError(e);
+  }
+}
+
+void getArticle(int articleId, StreamController<ArticleFull> controller) async {
+  final params = {
+    'method': 'getArticle',
+    'article_id': '$articleId',
+    'source_platform': 'grii_resmi',
+    'source_installation_id': getInstallationId(),
+    'source_app_packageName': 'yuku.grii',
+    'source_app_versionCode': '0',
+    'source_app_debug': '0',
+  };
+
+  final client = http.Client();
+
+  try {
+    final resp = await client.get(Flavor.current.pillarApiUrl + '?' + _encodeParams(params));
+    if (resp.statusCode == 200) {
+      final root = json.decode(resp.body);
+      final dynamic item = root['article'];
+      controller.add(ArticleFull(name: item['name'], title: item['title'], body: item['body']));
       controller.close();
     } else {
       throw Exception("bad status code ${resp.statusCode}");
